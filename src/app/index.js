638c388815226190f48acc5e2f245d219280bf5c
Vagan Mkrtchyan <v.mkrtchyan@i-sys.ru>
2020-07-26 10:04:46 +0400

    fix buttons next prev

--- src/app/index.js
+++ src/app/index.js
@@ -1,16 +1,17 @@
 import { createSlider } from './slider'
 import '../styles/style.css'
 
-const sliderContainer = document.getElementById('slider-container')
-const sliderContainer2 = document.getElementById('slider-container-2')
-
-createSlider(sliderContainer, {
-    numberOfElements: 4,
-    hideControls: true,
-    size: 740
-})
-createSlider(sliderContainer2, {
-    numberOfElements: 4,
-    size: 940,
-    timeout: 2000
-})
+document.addEventListener("DOMContentLoaded", function (event) {
+    const slider = document.getElementById('slider')
+    const slider2 = document.getElementById('slider-2')
+    createSlider(slider, {
+        numberOfElements: 4,
+        hideControls: true,
+        size: 740
+    })
+    createSlider(slider2, {
+        numberOfElements: 4,
+        size: 940,
+        timeout: 2000
+    })
+});
