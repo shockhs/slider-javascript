4b645eff2ec946cc58b87b3f5f1fdbb3d7f63dc7
Vagan Mkrtchyan <v.mkrtchyan@i-sys.ru>
2020-08-16 13:04:50 +0400

    jest fixes

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,67 +1,45 @@
-const position = require('../position')
+import { getNextPosition } from '../utils'
 
-const getNextPosition = position.getNextPosition
-
-describe('position', () => {
+describe('utils/getNextPosition', () => {
     const numberOfElements = 4
 
-    it('Returns the positions after tick in direction to the right at non-border values', () => {
+    it('Returns the position after tick in direction to the right at non-border values', () => {
         const prevNumber = 3,
             currentNumber = 0,
             nextNumber = 1
 
-        expect(getNextPosition(prevNumber, currentNumber, nextNumber, numberOfElements, 'right')).toStrictEqual({
-            prevNumber: 0,
-            nextNumber: 2,
-            currentNumber: 1,
-        })
+        expect(getNextPosition({ prevNumber, currentNumber, nextNumber }, numberOfElements, 'right')).toBe(1)
     })
 
-    it('Returns the positions on the pre-last image of slider after tick in direction to the right', () => {
+    it('Returns the position on the pre-last image of slider after tick in direction to the right', () => {
         const prevNumber = 1,
             currentNumber = 2,
             nextNumber = 3
 
-        expect(getNextPosition(prevNumber, currentNumber, nextNumber, numberOfElements, 'right')).toStrictEqual({
-            prevNumber: 2,
-            nextNumber: 0,
-            currentNumber: 3,
-        })
+        expect(getNextPosition({ prevNumber, currentNumber, nextNumber }, numberOfElements, 'right')).toBe(3)
     })
 
-    it('Returns the positions on the last image of slider after tick in direction to the right', () => {
+    it('Returns the position on the last image of slider after tick in direction to the right', () => {
         const prevNumber = 2,
             currentNumber = 3,
             nextNumber = 0
 
-        expect(getNextPosition(prevNumber, currentNumber, nextNumber, numberOfElements, 'right')).toStrictEqual({
-            prevNumber: 3,
-            nextNumber: 1,
-            currentNumber: 0,
-        })
+        expect(getNextPosition({ prevNumber, currentNumber, nextNumber }, numberOfElements, 'right')).toBe(0)
     })
 
-    it('Returns the positions at first image after tick in direction to the left', () => {
+    it('Returns the position at first image after tick in direction to the left', () => {
         const prevNumber = 3,
             currentNumber = 0,
             nextNumber = 1
 
-        expect(getNextPosition(prevNumber, currentNumber, nextNumber, numberOfElements, 'left')).toStrictEqual({
-            prevNumber: 2,
-            nextNumber: 0,
-            currentNumber: 3,
-        })
+        expect(getNextPosition({ prevNumber, currentNumber, nextNumber }, numberOfElements, 'left')).toBe(3)
     })
 
-    it('Returns the positions after tick in direction to the left at non-border values', () => {
+    it('Returns the position after tick in direction to the left at non-border values', () => {
         const prevNumber = 2,
             currentNumber = 3,
             nextNumber = 0
 
-        expect(getNextPosition(prevNumber, currentNumber, nextNumber, numberOfElements, 'left')).toStrictEqual({
-            prevNumber: 1,
-            nextNumber: 3,
-            currentNumber: 2,
-        })
+        expect(getNextPosition({ prevNumber, currentNumber, nextNumber }, numberOfElements, 'left')).toBe(2)
     })
 })
